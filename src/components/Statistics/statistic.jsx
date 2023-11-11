9ff3b502b6a80d30226fc8ccc411d80390bf2637
Viktoriya <arizona081392@gmail.com>
2023-11-11 22:57:56 +0200

    ok

--- src/components/Statistics/statistic.jsx
+++ src/components/Statistics/statistic.jsx
@@ -14,7 +14,7 @@ function randomColor() {
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>Upload stats</h2>
+      {title && <h2 className={css.title}></h2>}
 
       <ul className={css.statList}> {stats.map(({ id, label, percentage }) => {
                     return (
